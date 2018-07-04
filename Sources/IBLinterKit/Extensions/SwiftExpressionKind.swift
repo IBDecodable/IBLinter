413c36422c1bfd4c3bc2a50d1ad274fd05ea09c9
FUKAGAWA Satoru <sflocal2015@gmail.com>
2018-07-04 10:44:48 +0900

    Add SwiftLint Extension

---
+++ Sources/IBLinterKit/Extensions/SwiftExpressionKind.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public enum SwiftExpressionKind: String {
+    case call = "source.lang.swift.expr.call"
+    case argument = "source.lang.swift.expr.argument"
+    case array = "source.lang.swift.expr.array"
+    case dictionary = "source.lang.swift.expr.dictionary"
+    case objectLiteral = "source.lang.swift.expr.object_literal"
+}
