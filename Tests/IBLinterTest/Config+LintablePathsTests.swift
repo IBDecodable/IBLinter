b9528ca08025ef319cd2d3e0cd9418fee19daade
Yuta Saito <kateinoigakukun@gmail.com>
2019-02-15 01:18:32 +0900

    Fix lintable path

--- Tests/IBLinterTest/Config+LintablePathsTests.swift
+++ Tests/IBLinterTest/Config+LintablePathsTests.swift
@@ -8,17 +8,43 @@ class ConfigLintablePathsTests: XCTestCase {
     func testIncluded() {
         let config = Config(
             disabledRules: [], enabledRules: [],
-            excluded: ["Level1_1"], included: ["Level1_1/Level2_1"],
+            excluded: ["Level1_1"], included: [],
             customModuleRule: [], reporter: ""
         )
         let projectPath = bundleURL.appendingPathComponent("ProjectMock")
         let lintablePaths = config.lintablePaths(workDirectory: projectPath, fileExtension: "xib")
 
         XCTAssertEqual(
-            lintablePaths.map { $0.path }.sorted(),
-            [projectPath.appendingPathComponent("Level1_1/Level2_1/Level2_1.xib"),
-             projectPath.appendingPathComponent("Level1_2/Level1_2.xib")].map { $0.path }.sorted()
+            lintablePaths.map { $0.path },
+            [projectPath.appendingPathComponent("Level1_2/Level1_2.xib").path]
         )
+    }
+
+    func testExcluded() {
+        let config = Config(
+            disabledRules: [], enabledRules: [],
+            excluded: [], included: ["Level1_2"],
+            customModuleRule: [], reporter: ""
+        )
+        let projectPath = bundleURL.appendingPathComponent("ProjectMock")
+        let lintablePaths = config.lintablePaths(workDirectory: projectPath, fileExtension: "xib")
+
+        XCTAssertEqual(
+            lintablePaths.map { $0.path },
+            [projectPath.appendingPathComponent("Level1_2/Level1_2.xib").path]
+        )
+    }
+
+    func testIncludedAndExcluded() {
+        let config = Config(
+            disabledRules: [], enabledRules: [],
+            excluded: ["Level1_1"], included: ["Level1_1/Level2_1"],
+            customModuleRule: [], reporter: ""
+        )
+        let projectPath = bundleURL.appendingPathComponent("ProjectMock")
+        let lintablePaths = config.lintablePaths(workDirectory: projectPath, fileExtension: "xib")
+
+        XCTAssertEqual(lintablePaths, [])
     }
 }
 
