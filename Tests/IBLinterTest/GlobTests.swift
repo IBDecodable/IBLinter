fe00426d2036dadfa9dbaff5ff4345190a2cfe4f
Yuta Saito <kateinoigakukun@gmail.com>
2019-02-16 11:13:56 +0900

    Implement file system mock

--- Tests/IBLinterTest/GlobTests.swift
+++ Tests/IBLinterTest/GlobTests.swift
@@ -72,71 +72,98 @@ class GlobTests: XCTestCase {
     }
 
     class MockFileManager: GlobFileManager {
+        enum Path: Hashable {
+            case root(Set<Path>)
+            case directory(String, Set<Path>)
+            case file(String)
+            func hash(into hasher: inout Hasher) {
+                name.hash(into: &hasher)
+            }
+            var name: String {
+                switch self {
+                case .directory(let name, _), .file(let name): return name
+                case .root: return "__root__"
+                }
+            }
+            func find<C: Collection>(path: C) -> Path? where C.Element == String {
+                guard let firstPath = path.first else {
+                    if case .root = self { return self }
+                    return nil
+                }
+                let restPath = path.dropFirst()
+                switch self {
+                case .directory(let name, _) where name == firstPath && restPath.isEmpty:
+                    return self
+                case .directory(let name, let children) where name == firstPath:
+                    return children.lazy.compactMap { $0.find(path: path.dropFirst()) }.first
+                case .file(let name) where name == firstPath && restPath.isEmpty:
+                    return self
+                case .file(let name) where name == firstPath:
+                    return nil
+                case .root(let children):
+                    return children.lazy.compactMap { $0.find(path: path) }.first
+                default: return nil
+                }
+            }
+        }
         let projectPath: URL
-        init(projectPath: URL) {
+        init(projectPath: URL, tree: Path) {
             self.projectPath = projectPath
+            self.tree = tree
+        }
+        let tree: Path
+        enum Error: Swift.Error {
+            case noSuchPath
         }
         func subpathsOfDirectory(atPath path: String) throws -> [String] {
-            switch URL(fileURLWithPath: path).path {
-            case projectPath.path:
-                return [
-                    "Level1_1",
-                    "Level1_2",
-                ]
-            case projectPath.appendingPathComponent("Level1_1/").path:
-                return [
-                    "Level1_1.xib",
-                    "Level2_1",
-                    "Level2_2",
-                ]
-            case projectPath.appendingPathComponent("Level1_2/").path:
-                return ["Level1_2.xib"]
-            case projectPath.appendingPathComponent("Level1_1/Level2_1").path:
-                return ["Level2_1.xib"]
-            case projectPath.appendingPathComponent("Level1_1/Level2_2").path:
-                return ["Level2_2.xib"]
-            default:
-                XCTFail("\(path) is unexpected directory.")
-                return []
+            var path = path
+            guard let relativePathRange = path.range(of: projectPath.path) else { fatalError() }
+            path.removeSubrange(relativePathRange)
+            guard let treePath = tree.find(path: path.split(separator: "/").map(String.init)) else {
+                throw Error.noSuchPath
+            }
+            switch treePath {
+            case .directory(_, let children), .root(let children):
+                return children.map { $0.name }
+            case .file: fatalError()
             }
         }
 
         func isDirectory(_ url: String) -> Bool {
-            switch URL(fileURLWithPath: url).path {
-            case projectPath.path:
-                return true
-            case projectPath.appendingPathComponent("Level1_1/").path:
-                return true
-            case projectPath.appendingPathComponent("Level1_1/Level1_1.xib").path:
+            var path = url
+            guard let relativePathRange = path.range(of: projectPath.path) else { fatalError() }
+            path.removeSubrange(relativePathRange)
+            guard let treePath = tree.find(path: path.split(separator: "/").map(String.init)) else {
                 return false
-            case projectPath.appendingPathComponent("Level1_1/Level2_1/").path:
-                return true
-            case projectPath.appendingPathComponent("Level1_1/Level2_2/").path:
-                return true
-            case projectPath.appendingPathComponent("Level1_2/").path:
-                return true
-            case projectPath.appendingPathComponent("Level1_2/Level1_2.xib").path:
-                return false
-            case projectPath.appendingPathComponent("Level1_1/Level2_1/").path:
-                return true
-            case projectPath.appendingPathComponent("Level1_1/Level2_1/Level2_1.xib").path:
-                return false
-            case projectPath.appendingPathComponent("Level1_1/Level2_2/").path:
-                return true
-            case projectPath.appendingPathComponent("Level1_1/Level2_2/Level2_2.xib").path:
-                return false
-            default:
-                XCTFail("\(url) is unexpected directory.")
-                return false
+            }
+            switch treePath {
+            case .file: return false
+            case .directory, .root: return true
             }
         }
     }
 
     func testExpandGlobstar() {
-        let projectPath = bundleURL.appendingPathComponent("ProjectMock")
+        let projectPath = URL(fileURLWithPath: "./Project")
         do {
             let multiRecursivePath = projectPath.appendingPathComponent("**/Level2_1/**")
-            let expanded = expandGlobstar(pattern: multiRecursivePath.path, fileManager: MockFileManager(projectPath: projectPath))
+            let fileManager = MockFileManager(
+                projectPath: projectPath,
+                tree: .root(
+                    [
+                        .directory(
+                            "Level1_1",
+                            [
+                                .file("Level1_1.xib"),
+                                .directory("Level2_1", [.file("Level2_1.xib")]),
+                                .directory("Level2_2", [.file("Level2_2.xib")])
+                            ]
+                        ),
+                        .directory("Level1_2", [.file("Level1_2.xib")])
+                    ]
+                )
+            )
+            let expanded = expandGlobstar(pattern: multiRecursivePath.path, fileManager: fileManager)
             XCTAssertEqual(
                 expanded.map { URL(fileURLWithPath: $0).path }.sorted(),
                 [
