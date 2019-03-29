d3550cd7f46a54abb096f1b4d23e8c9114520f8d
Shinichi Hosokawa <shin.hosokawa@gmail.com>
2019-03-29 10:08:56 +0900

    Adds rules to keep "View as" device (#86)
    * Adds a rule view_as_device

--- Tests/IBLinterKitTest/ConfigTest.swift
+++ Tests/IBLinterKitTest/ConfigTest.swift
@@ -21,4 +21,12 @@ class ConfigTest: XCTestCase {
         XCTAssertEqual(config.enabledRules, [])
         XCTAssertEqual(config.excluded, ["Carthage"])
     }
+
+    func testViewAsDeviceConfigFile() throws {
+        let url = self.url(forResource: ".iblinter_view_as_device", withExtension: "yml")
+        let workingDirectory = url.deletingLastPathComponent()
+        let config = try Config.load(from: workingDirectory, fileName: url.lastPathComponent)
+        XCTAssertNotNil(config.viewAsDeviceRule)
+        XCTAssertEqual(config.viewAsDeviceRule?.deviceId, "retina3_5")
+    }
 }
