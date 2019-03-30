5bf8f8dd06eb1e7d06347e1b006a8f1ecbfcf2ca
Yuta Saito <kateinoigakukun@gmail.com>
2019-03-30 14:15:14 +0900

    Update comments

--- Tests/IBLinterKitTest/Supports/Context+Mock.swift
+++ Tests/IBLinterKitTest/Supports/Context+Mock.swift
@@ -4,7 +4,7 @@ import Foundation
 extension Context {
     static func mock(from config: Config) -> Context {
         let workDirectory = URL(fileURLWithPath: #file)
-            .deletingLastPathComponent() // ./Tests/IBLinterKitTests/Rules
+            .deletingLastPathComponent() // ./Tests/IBLinterKitTests/Supports
             .deletingLastPathComponent() // ./Tests/IBLinterKitTests
             .deletingLastPathComponent() // ./Tests
             .deletingLastPathComponent() // ./
