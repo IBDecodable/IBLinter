dc12385b0b7b5e5d000ff05d2644052b4bfdb38f
giginet <giginet.net@gmail.com>
2019-04-15 17:58:36 +0900

    Add test to use XibFile

---
+++ Tests/IBLinterKitTest/ValidatorTest.swift
@@ -0,0 +1,56 @@
+import Foundation
+import XCTest
+import IBDecodable
+@testable import IBLinterKit
+
+private struct CheckXibRule: Rule {
+    init(context: Context) { }
+    
+    static var identifier: String = "check_xib"
+    static var description: String = "Warn xib"
+    
+    func validate(storyboard: StoryboardFile) -> [Violation] {
+        return []
+    }
+    
+    func validate(xib: XibFile) -> [Violation] {
+        return [
+            Violation(pathString: xib.pathString, message: "This is xib", level: .error)
+        ]
+    }
+}
+
+private struct CheckStoryboardRule: Rule {
+    init(context: Context) { }
+    
+    static var identifier: String = "check_storyboard"
+    static var description: String = "Warn storyboard"
+    
+    func validate(storyboard: StoryboardFile) -> [Violation] {
+        return [
+            Violation(pathString: storyboard.pathString, message: "This is storyboard", level: .error)
+        ]
+    }
+    
+    func validate(xib: XibFile) -> [Violation] {
+        return []
+    }
+}
+
+final class VaridatorTest: XCTestCase {
+    func testValidateXib() {
+        let workingDir = Fixture().path("Resources/Validator")
+        let validator = Validator(externalRules: [CheckXibRule.self])
+        let config = try! Config.load(workingDir.appendingPathComponent(".iblinter.yml"))
+        let violations = validator.validate(workDirectory: workingDir, config: config)
+        XCTAssertEqual(violations.count, 1)
+    }
+    
+    func testValidateStoryboard() {
+        let workingDir = Fixture().path("Resources/Validator")
+        let validator = Validator(externalRules: [CheckStoryboardRule.self])
+        let config = try! Config.load(workingDir.appendingPathComponent(".iblinter.yml"))
+        let violations = validator.validate(workDirectory: workingDir, config: config)
+        XCTAssertEqual(violations.count, 1)
+    }
+}
