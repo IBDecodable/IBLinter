631cd7aeb8d3aa249927d26758df83e238d48d7e
Joel Loeshelle <jloeshelle@alarm.com>
2019-12-27 09:05:40 -0500

    Style changes requested in https://github.com/IBDecodable/IBLinter/pull/135

--- Sources/IBLinterFrontend/Commands/ValidateCommand.swift
+++ Sources/IBLinterFrontend/Commands/ValidateCommand.swift
@@ -58,13 +58,13 @@ struct ValidateOptions: OptionsProtocol {
     let iblinterFilePath: String?
     let configurationFile: String?
     let included: [String]
+
     static func create(_ path: String?) -> (_ reporter: String?) -> (_ script: String?) -> (_ config: String?) -> (_ included: String?) -> ValidateOptions {
         return { reporter in
             return { script in
                 return { config in
                     return { included in
                         let finalIncluded = included?.split { $0.isNewline || $0 == "," }.map { String($0) }
-
                         return self.init(path: path, reporter: reporter, iblinterFilePath: script, configurationFile: config, included: finalIncluded ?? [])
                     }
                 }
@@ -78,8 +78,7 @@ struct ValidateOptions: OptionsProtocol {
             <*> mode <| Option(key: "reporter", defaultValue: nil, usage: "the reporter used to log errors and warnings")
             <*> mode <| Option(key: "script", defaultValue: nil, usage: "custom IBLinterfile.swift")
             <*> mode <| Option(key: "config", defaultValue: nil, usage: "the path to IBLint's configuration file")
-            // swiftlint:disable:next line_length
-            <*> mode <| Option(key: "included", defaultValue: nil, usage: "included files/paths to lint. This is ignored if you specified included paths in your yml configuration file. You can separate paths using `,` or a new line")
+            <*> mode <| Option(key: "included", defaultValue: nil, usage: "included files/paths to lint. This is ignored if you specified included paths in your yml configuration file. You can separate paths using `,` or a new line") //swiftlint:disable:this line_length
     }
 }
 
