8e085574c295bca88e2c2fa0ac8d22fe09b7e4b8
ykkd <17854586+ykkd@users.noreply.github.com>
2022-02-06 15:56:28 +0900

    implement validation for HidesBottomBarRule

--- Sources/IBLinterKit/Rules/HidesBottomBarRule.swift
+++ Sources/IBLinterKit/Rules/HidesBottomBarRule.swift
@@ -14,15 +14,29 @@ extension Rules {
         static var identifier = "hides_bottom_bar"
         static var description = "Check if hidesBottomBarWhenPushed is enabled"
         
-        init(context: Context) {
-            // TODO: implement
-        }
+        private let excluded: [String]
         
-        func validate(storyboard: StoryboardFile) -> [Violation] {
-            // TODO: implement
-            return []
+        init(context: Context) {
+            excluded = context.config.hidesBottomBarRule?.excludedViewControllers ?? []
         }
         
         func validate(xib: XibFile) -> [Violation] { return [] }
+        
+        func validate(storyboard: StoryboardFile) -> [Violation] {
+            guard let scenes = storyboard.document.scenes else { return [] }
+            let viewControllers = scenes.compactMap { $0.viewController?.viewController }
+            return viewControllers.compactMap { validate(for: $0, file: storyboard) }
+        }
+        
+        private func validate<T: InterfaceBuilderFile>(for viewController: ViewControllerProtocol, file: T) -> Violation? {
+            guard let hidesBottomBar = viewController.hidesBottomBarWhenPushed,
+                  excluded.isEmpty,
+                  !excluded.contains(where: { $0 == viewController.customClass ?? viewController.elementClass })
+                   else {
+                return nil
+            }
+            let message = "\(viewController.customClass ?? viewController.elementClass).hidesBottomBarWhenPushed is not enabled. id: \(viewController.id)"
+            return hidesBottomBar ? nil : Violation(pathString: file.pathString, message: message, level: .error)
+        }
     }
 }
