e27be365f04245c8a0fa24129229f85e3d934881
ykkd <17854586+ykkd@users.noreply.github.com>
2022-02-07 00:35:53 +0900

    lint

--- Sources/IBLinterKit/Rules/HidesBottomBarRule.swift
+++ Sources/IBLinterKit/Rules/HidesBottomBarRule.swift
@@ -10,24 +10,24 @@ import IBDecodable
 extension Rules {
 
     struct HidesBottomBarRule: Rule {
-        
+
         static var identifier = "hides_bottom_bar"
         static var description = "Check if hidesBottomBarWhenPushed is enabled"
-        
+
         private let excluded: [String]
-        
+
         init(context: Context) {
             excluded = context.config.hidesBottomBarRule?.excludedViewControllers ?? []
         }
-        
+
         func validate(xib: XibFile) -> [Violation] { return [] }
-        
+
         func validate(storyboard: StoryboardFile) -> [Violation] {
             guard let scenes = storyboard.document.scenes else { return [] }
             let viewControllers = scenes.compactMap { $0.viewController?.viewController }
             return viewControllers.compactMap { validate(for: $0, file: storyboard) }
         }
-        
+
         private func validate<T: InterfaceBuilderFile>(for viewController: ViewControllerProtocol, file: T) -> Violation? {
             guard let customClass = viewController.customClass,
                   !excluded.contains(where: { $0 == customClass }) else {
